7e81863e4dd3d2067bfbfd7f881b934d78d43cc5
Tamojit Das <tamojitdas181007@gmail.com>
2024-07-14 16:21:04 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/api/v1", (req, res) => {
+    res.send("<h1>Sthethology API V1</h1>");
+});
+
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is listening on Port: ${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Courses from "./pages/Courses"
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Courses />
+      </div>
+    </>
+  )
+}
+
+export default App
